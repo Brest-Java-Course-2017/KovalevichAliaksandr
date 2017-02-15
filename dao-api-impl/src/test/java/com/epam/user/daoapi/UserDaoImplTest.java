b5c6281b1ba3c32b9c522f02b504a2fd56e198bd
KovalevichAliaksandr <servistest@gmail.com>
2017-02-15 20:58:02 +0300

    add module test-db

---
+++ dao-api-impl/src/test/java/com/epam/user/daoapi/UserDaoImplTest.java
@@ -0,0 +1,41 @@
+package com.epam.user.daoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alex on 15.2.17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:test-spring-dao.xml"})
+public class UserDaoImplTest {
+
+    @Test
+    public void findUserById() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void findAllUsers() throws Exception {
+
+    }
+}
